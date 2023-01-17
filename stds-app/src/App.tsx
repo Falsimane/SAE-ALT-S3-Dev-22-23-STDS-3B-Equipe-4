efd5e8acc7b9e09f68926f26e3c60019722e1e60
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-01-17 11:42:17 +0100

    app modif

--- stds-app/src/App.tsx
+++ stds-app/src/App.tsx
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import CircleIcon from "@mui/icons-material/Circle";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+
+function CardBox(props: { colordot: string; title: string }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}>
+      <Box sx={{ width: "90%" }}>
+        <Typography
+          sx={{
+            textAlign: "left",
+            fontWeight: "bold",
+            fontSize: 20,
+            marginBottom: 1
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {" "}
+          {props.title}{" "}
+        </Typography>
+        
+
+          <Card sx={{ backgroundColor: "#E6E6E6", height: 120 }}>
+            <CardActionArea sx={{height: '100%'}}>
+              <CardContent>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                marginRight: 1,
+                marginTop: 1
+              }}
+            >
+              <CircleIcon sx={{ color: props.colordot }} />
+            </Box>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+        
+      </Box>
+    </Box>
   );
 }
 
-export default App;
+export default function App() {
+  const colordotTemp = "#22B04B";
+  const colordotQuant = "#F49229";
+  const colordotPuiss = "#ED1C24";
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className="App">
+      <AppBar sx={{ position: "static", backgroundColor: "#ffbe76" }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleMenu}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon sx={{ color: "#000" }} />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "block", color: "#000" },
+              fontWeight: "bold",
+              fontStyle: "italic",
+              fontSize: 25
+            }}
+          >
+            STDS
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Menu 
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        PaperProps={{
+          sx: {
+            bgcolor: "#F6E58D"
+          }
+        }}
+      >
+        <MenuItem onClick={handleClose}>Accueil</MenuItem>
+        <MenuItem onClick={handleClose}>Maintenance</MenuItem>
+      </Menu>
+
+      <CardBox colordot={colordotTemp} title={"Température"} />
+      <CardBox colordot={colordotQuant} title={"Quantité fût"} />
+      <CardBox colordot={colordotPuiss} title={"Puissance"} />
+    </div>
+  );
+}
