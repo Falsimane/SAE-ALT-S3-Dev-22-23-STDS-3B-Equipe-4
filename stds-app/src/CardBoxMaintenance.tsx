548fde0cbcc3d56887996383e6e5f434db58a8c0
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-01-18 08:20:26 +0100

    commit maintenance

---
+++ stds-app/src/CardBoxMaintenance.tsx
@@ -0,0 +1,32 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CircleIcon from "@mui/icons-material/Circle";
+import * as React from "react";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+
+export default function CardBoxMaintenance(props: { titre:string ; pdfimg: boolean}) {
+    return (
+        <Box sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}>
+            <Box sx={{ width: "80%" }}>
+                
+
+
+                <Card sx={{ backgroundColor: "#E6E6E6", height: 120}}>
+                    <CardActionArea sx={{height: '100%'}}>
+                        <CardContent sx= {{ display: 'flex'}}>
+                           
+                            <Typography sx = {{ fontSize: 30, marginLeft: 10}}> {props.titre} </Typography>
+                            
+                            <ArrowForwardIosIcon fontSize ='large' sx={{position: 'absolute', right : 30}}/>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+
+            </Box>
+        </Box>
+    );
+}
--- stds-app/src/Maintenance.tsx
+++ stds-app/src/Maintenance.tsx
@@ -1,11 +1,18 @@
 
 import * as React from "react";
 import Header from "./Header";
+import CardBoxMaintenance from "./CardBoxMaintenance";
 
 export default function Maintenance() {
     return (
         <div className="Maintenance">
             <Header page={"Maintenance"} dimensionsTitre={130}/>
+            <CardBoxMaintenance titre="Notice d'utilisation" pdfimg = {true} /> 
+            <CardBoxMaintenance titre="Manuel de service" pdfimg = {true} /> 
+            <CardBoxMaintenance titre="Maintenance préventive" pdfimg = {false}/>
+            <CardBoxMaintenance titre="Maintenance curative" pdfimg = {false}/> 
+
+            
         </div>
     );
 }
