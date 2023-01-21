91ef9c81e6ebda1276701cd62e017cf11af664f9
NolanJacquemont <nolan.jacquemont@gmail.com>
2023-01-21 13:43:54 +0100

    update websocket connection, Ref #25
    > 2 websockets par topic sont créés, à fix

--- Application/stds-app/src/utils/mqtt/hooks/useWebsocket.ts
+++ Application/stds-app/src/utils/mqtt/hooks/useWebsocket.ts
@@ -2,14 +2,23 @@ import React, {useEffect} from "react";
 
 const useWebsocket = <T>(path: string, defaultValue: T) => {
     const [val, setVal] = React.useState(defaultValue);
+    const [isConnected, setIsConnected] = React.useState(false);
 
     useEffect(() => {
+        if (isConnected) {
+            return;
+        }
         const websocket = new WebSocket('ws://127.0.0.1:8080/ws/' + path);
+        websocket.onopen = () => {
+            setIsConnected(true);
+        }
         websocket.onmessage = (event) => {
             setVal(event.data);
-            websocket.close();
         }
-    }, [path]);
+        websocket.onclose = () => {
+            setIsConnected(false);
+        }
+    }, [isConnected, path]);
 
     return val;
 }
