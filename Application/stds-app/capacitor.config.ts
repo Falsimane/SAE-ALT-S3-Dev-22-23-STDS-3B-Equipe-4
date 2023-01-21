9d88a3b0c153a50924e8933ad896c136b73789b9
NolanJacquemont <nolan.jacquemont@gmail.com>
2023-01-21 15:47:11 +0100

    fix appId

--- Application/stds-app/capacitor.config.ts
+++ Application/stds-app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'fr.iut-blagnac.stds-app',
+  appId: 'fr.iutblagnac.stdsapp',
   appName: 'STDS App',
   webDir: 'build',
   bundledWebRuntime: false
