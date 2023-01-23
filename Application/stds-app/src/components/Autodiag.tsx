103de8ad4421e85511f3f7a94ada5e8ea4bf7ace
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-01-23 08:29:49 +0100

    Autodiagnostique ref #16

---
+++ Application/stds-app/src/components/Autodiag.tsx
@@ -0,0 +1,50 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CircleIcon from "@mui/icons-material/Circle";
+import * as React from "react";
+import CardBoxMaintenance from "../components/CardBoxMaintenance";
+import {IonContent} from "@ionic/react";
+import {useContext} from "react";
+import STDSDatasContext from "../utils/mqtt/STDSDatasContext";
+import {useHistory} from "react-router";
+
+
+
+
+export default function Autodiag() {
+
+    
+
+     /*
+    switch (diagnostique){
+        case "Température élevée de la bière !" :
+            
+            break
+
+       case "Temperature élevée de la bière !" :
+            history.push("/pdf/gamme-02")
+            break
+
+        case "Temperature élevée de la bière !" :
+            history.push("/pdf/gamme-03")
+            break
+
+        case "Temperature élevée de la bière !" :
+            history.push("/pdf/gamme-04")
+            break
+
+        case "Temperature élevée de la bière !" :
+            history.push("/pdf/gamme-05")
+            break
+
+        case "Temperature élevée de la bière !" :
+            history.push("/pdf/gamme-06")
+            break
+        
+    
+        }
+*/
+    }
