591d963e3f27895e235659c1c97db8083f92a14a
Falsimane <falsimagne.thomas@gmail.com>
2023-01-23 11:33:11 +0100

    ADD : ButtonComposant

--- Application/stds-app/src/components/BarChart.tsx
+++ Application/stds-app/src/components/BarChart.tsx
@@ -14,8 +14,6 @@ export default function BarChart(){
             console.log(pfResponse);
         });
     }
-    fetchData();
-
 
 
 
--- Application/stds-app/src/utils/db/GetData.tsx
+++ Application/stds-app/src/utils/db/GetData.tsx
@@ -1,7 +1,5 @@
 import {InfluxDB} from '@influxdata/influxdb-client'
 
-{/* Function to get the hour and the value that correspond from the database */}
-
 const getData = (_measurement: any, _range: string) => {
     const url = 'http://localhost:8086';
     const token = 'EBE859CD-7EC2-438A-8185-C5856E591C56';
