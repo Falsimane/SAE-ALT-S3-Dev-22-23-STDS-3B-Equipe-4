d11c315915816ab1b7734c23e1e79a64be8a7e03
Falsimane <falsimagne.thomas@gmail.com>
2023-01-23 16:13:00 +0100

    ADD: BarChart Ref #18

--- Application/stds-app/src/pages/Temperature.tsx
+++ Application/stds-app/src/pages/Temperature.tsx
@@ -22,7 +22,7 @@ export default function Temperature() {
                 </IonRefresher>
 
                 <Box sx={{ height:"20%", width: "90%", backgroundColor:"#E6E6E6", marginLeft: "5%", marginTop: 3, borderRadius:1}}>
-                    <Box sx={{height:"3%", width: "10%", backgroundColor:"E6E6E6", marginLeft: "70%", marginTop: 0}}>
+                    <Box sx={{height:"3%", width: "5%", backgroundColor:"E6E6E6", marginLeft: "70%", marginTop: 0}}>
                         <Button />
                     </Box>
                     <BarChart />
