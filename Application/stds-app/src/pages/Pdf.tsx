e395d47d0fa7e04785e246fad944b2a6ea4318e3
James <100424900+Darknxss@users.noreply.github.com>
2023-01-26 10:30:12 +0100

    commit big fix PDF

--- Application/stds-app/src/pages/Pdf.tsx
+++ Application/stds-app/src/pages/Pdf.tsx
@@ -1,13 +1,6 @@
-import React, { useState } from "react";
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import {Button} from "@mui/material";
+import React from "react";
 import {IonContent, IonPage, IonRefresher, IonRefresherContent} from "@ionic/react";
-import Header from "./Header";
 import {useParams} from "react-router-dom";
-import PdfNoticeUtilisation from "../documents/STDS_notice_utilisation.pdf";
 import PdfManuelService from "../documents/STDS_manuel_service.pdf";
 import PdfGammePreventive1 from "../documents/gammes/gamme_preventive_1.pdf";
 import PdfGammePreventive2 from "../documents/gammes/gamme_preventive_2.pdf";
@@ -17,10 +10,8 @@ import PdfGammeCurative3 from "../documents/gammes/gamme_curative_3.pdf";
 import PdfGammeCurative4 from "../documents/gammes/gamme_curative_4.pdf";
 import PdfGammeCurative5 from "../documents/gammes/gamme_curative_5.pdf";
 import PdfGammeCurative6 from "../documents/gammes/gamme_curative_6.pdf";
-import '../theme/Pdf.css';
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+// @ts-ignore
+import { MobilePDFReader } from 'react-read-pdf';
 
 export default function Pdf() {
 
@@ -31,27 +22,13 @@ export default function Pdf() {
     };
 
     const { id } : {id: string} = useParams();
-    const [numPages, setNumPages] = useState(1);
-    const [pageNumber, setPageNumber] = useState(1);
-
-    function onDocumentLoadSuccess({ numPages } : {numPages:any}) {
-        setNumPages(numPages);
-    }
-
-    const openPagePrec = () =>
-        setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
-
-    const openPageSuiv = () =>
-        setPageNumber(
-            pageNumber + 1 >= numPages ? numPages : pageNumber + 1,
-        );
 
     let titre = "";
     let pdf = null;
     switch(id){
         case "notice-utilisation":
             titre = "Notice d'utilisation";
-            pdf = PdfNoticeUtilisation;
+            //pdf = PdfNoticeUtilisation;
             break;
         case "manuel-service":
             titre = "Manuel de service";
@@ -97,19 +74,9 @@ export default function Pdf() {
                 <IonRefresher slot="fixed" onIonRefresh={refresh}>
                     <IonRefresherContent></IonRefresherContent>
                 </IonRefresher>
-                <Header page={titre} dimensionsTitre={200}/>
-                <Box sx={{ display: "flex", justifyContent: "center", marginBottom: 3}}>
-                    <Box>
-                        <Box sx={{display: "flex", alignItems: "center", textAlign: "center", marginTop: 5, marginBottom: 5}}>
-                            <Button onClick={openPagePrec} disabled={pageNumber <= 1} sx={{marginLeft: 3, backgroundColor: "rgba(0,0,0,0.05)", width: "100%", fontWeight: "bold"}}>Précédent</Button>
-                            <Typography sx={{width: "100%", fontWeight: "bold"}}> {pageNumber} / {numPages}</Typography>
-                            <Button onClick={openPageSuiv} disabled={pageNumber >= numPages} sx={{backgroundColor: "rgba(0,0,0,0.05)", marginRight: 3, width: "100%", fontWeight: "bold"}}>Suivant</Button>
-                        </Box>
-                        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess} sx={{width: "20%"}}>
-                            <Page pageNumber={pageNumber}/>
-                        </Document>
-                    </Box>
-            </Box>
+                        <div style={{overflow:'scroll',height:600}}>
+                            <MobilePDFReader url="http://localhost:3000/STDS_notice_utilisation.pdf"/>
+                        </div>
             </IonContent>
         </IonPage>
     );
